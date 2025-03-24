7ef187ccf2b551710cbb91713d810cc4c4f6a713
Carlos Ribera Donet <carlosriberadonet@gmail.com>
2025-03-24 12:31:20 +0100

    arreglo la seccion formaciones

--- src/components/Formaciones.js
+++ src/components/Formaciones.js
@@ -1,45 +1,46 @@
-import React from "react";
-import "../styles/Formaciones.css";
-import eneagrama from "../assets/images/eneagrama.png";
-import coaching from "../assets/images/coaching.png";
-import eneacoaching from "../assets/images/eneacoaching.png";
-import '../styles/global/ButtonStyles.css';
+import React from 'react';
 
+import '../styles/Formaciones.css';
+
+import eneacoaching from '../assets/images/eneacoaching.png';
+import coaching from '../assets/images/coaching.png';
+import eneagrama from '../assets/images/eneagrama.png';
 
 const formacionesData = [
   {
-    imagen: coaching,
-    titulo: "ENEACOACHING PROFESIONAL",
-    descripcion: "Conecta con tu esencia y con tu propósito profesional a través del Eneagrama y del Coaching Ontológico",
+    image: coaching,
+    title: "ENEACOACHING PARA EMPRESAS",
+    description: "Conecta con tu máximo potencial en tu empresa y alinea tus creencias, valores y fortalezas con tu equipo de trabajo",
     link: "#",
   },
   {
-    imagen: eneacoaching,
-    titulo: "ENEACOACHING PARA EMPRESAS",
-    descripcion: "Conecta con tu máximo potencial en tu empresa y alinea tus creencias, valores y fortalezas con tu equipo de trabajo",
+    image: eneacoaching,
+    title: "ENEACOACHING PROFESIONAL",
+    description: "Conecta con tu esencia y con tu propósito profesional a través del Eneagrama y del Coaching Ontológico",
     link: "#",
   },
   {
-    imagen: eneagrama,
-    titulo: "CURSO BÁSICO EN ENEAGRAMA",
-    descripcion: "iníciate en esta apasionante herramienta de autoconocimiento y desarrollo personal para conectar con tu máximo potencial",
+    image: eneagrama,
+    title: "CURSO BÁSICO EN ENEAGRAMA",
+    description: "Iníciate en esta apasionante herramienta de autoconocimiento y desarrollo personal para conectar con tu máximo potencial",
     link: "#",
   },
 ];
 
 const Formaciones = () => {
   return (
-    <section className="formaciones">
-      <h2>Próximas Formaciones</h2>
-      <div className="formaciones-container">
-        {formacionesData.map((formacion, index) => (
-          <div key={index} className="formacion-card">
-            <img src={formacion.imagen} alt={formacion.titulo} className="formacion-img"/>
-            <div className="formacion-info">
-              <h3>{formacion.titulo}</h3>
-              <p className="formacion-descripcion">{formacion.descripcion}</p>  {/*  Nueva línea con la descripción */}
-              <p>{formacion.fecha}</p>
-              <a href={formacion.link} className="btn-ver-mas">Ver más</a>
+    <section className="formacion-seccion">
+      <h2 className="formacion-titulo-seccion">Próximas Formaciones</h2>
+      <div className="formacion-contenedor">
+        {formacionesData.map((item, index) => (
+          <div className="formacion-tarjeta" key={index}>
+            <div className="formacion-imagen">
+              <img src={item.image} alt={item.title} />
+            </div>
+            <div className="formacion-contenido">
+              <h3 className="formacion-titulo">{item.title}</h3>
+              <p className="formacion-descripcion">{item.description}</p>
+              <a href={item.link} className="formacion-boton">Ver más</a>
             </div>
           </div>
         ))}
